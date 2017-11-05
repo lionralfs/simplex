7f54873b10fc606342728aa4cc68c1a044990a33
Lion Ralfs <lion.ralfs@hotmail.com>
2017-11-05 13:36:27 +0100

    Some modificationsn

--- main.go
+++ main.go
@@ -15,15 +15,30 @@ func main() {
 
 func solve(problem *mat.Dense) {
 	rows, cols := problem.Dims()
+	initialVars := cols - 1
+	constraints := rows - 1
+
+	fmt.Printf("We have %d initial variables.\n", initialVars)
+	fmt.Printf("We have %d constraints.\n", constraints)
 	result := mat.NewDense(rows, cols, nil)
 
-	// add slack variables
-	for i := 1; i < rows; i++ {
-		result.Set(i, cols-1, problem.At(i, cols-1))
-		for j := 0; j < cols-1; j++ {
-			fmt.Println(problem.At(i, j))
+	for i := 0; i < rows-1; i++ {
+		row := problem.RowView(i)
+		// set the last element as the first
+		result.Set(i, 0, row.At(row.Len()-1, 0))
+
+		for j := 0; j < row.Len()-1; j++ {
+			value := row.At(j, 0)
+			if i > 0 {
+				value *= -1
+			}
+			result.Set(i, j+1, value)
 		}
 	}
 
-	fmt.Println(*result)
+	// display result
+	resultRows, _ := result.Dims()
+	for i := 0; i < resultRows; i++ {
+		fmt.Println(result.RawRowView(i))
+	}
 }
