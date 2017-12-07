ec6338471cbdd221a8ea2540cfda7d32c95ebca8
Lion Ralfs <lion.ralfs@hotmail.com>
2017-12-07 22:18:10 +0100

    Refactoring, smarter structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # simplex-go
+
+The [simplex algorithm](https://en.wikipedia.org/wiki/Simplex_algorithm)
+implemented in Go.
--- main.go
+++ main.go
@@ -8,59 +8,54 @@ import (
 
 func main() {
 	// create new example matrix
-	maximize := mat.NewVecDense(4, []float64{19, 13, 12, 17})
+	maximize := mat.NewVecDense(3, []float64{5, 4, 3})
 
-	constraints := mat.NewDense(3, 5, []float64{3, 2, 1, 2, 225, 1, 1, 1, 1, 117, 4, 3, 3, 4, 420})
+	constraints := mat.NewDense(3, 4, []float64{
+		2, 3, 1, 5,
+		4, 1, 2, 11,
+		3, 4, 2, 8})
 
 	solve(maximize, constraints)
 }
 
 func solve(maximize mat.Vector, constraints *mat.Dense) {
-	rows, cols := constraints.Dims()
-	b := mat.NewVecDense(cols+rows, nil)
-	constraintValues := constraints.ColView(cols - 1)
-	for i := 0; i < rows; i++ {
-		b.SetVec(i, constraintValues.At(i, 0))
+
+	// original data
+	constraintCount, variablesCount := constraints.Dims()
+	totalVariables := constraintCount + variablesCount - 1
+
+	// construct matrix A
+	A := mat.DenseCopyOf(constraints.Grow(0, constraintCount-1))
+
+	// handle first vector manually to override b-values
+	tempVector := make([]float64, constraintCount, constraintCount)
+	for i := range tempVector {
+		tempVector[i] = 0
+	}
+	tempVector[0] = 1
+	A.SetCol(variablesCount-1, tempVector)
+
+	// we can start at 1, since we already handled the first vector
+	for i := 1; i < constraintCount; i++ {
+		A.Set(i, i+variablesCount-1, 1)
 	}
 
-	fmt.Printf("maximize vector:\n %v\n\n",
-		mat.Formatted(maximize, mat.Prefix(" "), mat.Excerpt(3)))
-
-	A := constraints.Slice(0, rows, 0, cols-1)
-	fmt.Printf("A matrix:\n %v\n\n",
-		mat.Formatted(A, mat.Prefix(" "), mat.Excerpt(3)))
-
-	B := mat.NewDense(rows, rows, nil)
-	for i := 0; i < rows; i++ {
-		B.Set(i, i, 1)
+	// construct c by copying values over
+	c := mat.NewDense(1, totalVariables, make([]float64, totalVariables, totalVariables))
+	for i := 0; i < maximize.Len(); i++ {
+		c.Set(0, i, maximize.At(i, 0))
 	}
-	fmt.Printf("B matrix:\n %v\n\n",
-		mat.Formatted(B, mat.Prefix(" "), mat.Excerpt(3)))
 
-	fmt.Printf("b vector:\n %v\n\n",
-		mat.Formatted(b, mat.Prefix(" "), mat.Excerpt(3)))
+	// construct b vector
+	bTemp := make([]float64, constraintCount, constraintCount)
+	for i := 0; i < constraintCount; i++ {
+		bTemp[i] = constraints.At(i, constraintCount)
+	}
+	b := mat.NewVecDense(constraintCount, bTemp)
 
-	// fmt.Printf("We have %d initial variables.\n", initialVars)
-	// fmt.Printf("We have %d constraints.\n", constraints)
-	// result := mat.NewDense(rows, cols, nil)
+	fmt.Printf("A matrix:\n %v\n\n", mat.Formatted(A, mat.Prefix(" "), mat.Excerpt(8)))
 
-	// for i := 0; i < rows-1; i++ {
-	// 	row := problem.RowView(i)
-	// 	// set the last element as the first
-	// 	result.Set(i, 0, row.At(row.Len()-1, 0))
+	fmt.Printf("c vector:\n %v\n\n", mat.Formatted(c, mat.Prefix(" "), mat.Excerpt(8)))
 
-	// 	for j := 0; j < row.Len()-1; j++ {
-	// 		value := row.At(j, 0)
-	// 		if i > 0 {
-	// 			value *= -1
-	// 		}
-	// 		result.Set(i, j+1, value)
-	// 	}
-	// }
-
-	// // display result
-	// resultRows, _ := result.Dims()
-	// for i := 0; i < resultRows; i++ {
-	// 	fmt.Println(result.RawRowView(i))
-	// }
+	fmt.Printf("b vector:\n %v\n\n", mat.Formatted(b, mat.Prefix(" "), mat.Excerpt(8)))
 }
